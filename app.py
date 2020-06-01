8ed877df7278cb5ccde2648ac1d81ac5e69fe184
VK <vkjoy779@gmail.com>
2020-06-01 12:45:31 +0530

    Water Notification

---
+++ app.py
@@ -0,0 +1,10 @@
+import time
+from plyer import notification
+
+while True:
+    notification.notify(
+        title="Drink Water",
+        message='Drink water to keep yourself hydrated',
+        app_icon=r'C:\Users\ramanarao.vepuri\VSCode\Projects\WaterNotify\glass.ico',
+        timeout=2)
+    time.sleep(60*60)

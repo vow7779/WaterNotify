133ed42e4a022ce466fedd87dc6ce3593c0469f6
vow7779 <66241362+vow7779@users.noreply.github.com>
2020-06-12 18:11:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,6 @@ while True:
     notification.notify(
         title="Drink Water",
         message='Drink water to keep yourself hydrated',
-        app_icon=r'C:\Users\ramanarao.vepuri\VSCode\Projects\WaterNotify\glass.ico',
+        app_icon=r'C:\Users\rv\VSCode\Projects\WaterNotify\glass.ico',
         timeout=2)
     time.sleep(60*60)
